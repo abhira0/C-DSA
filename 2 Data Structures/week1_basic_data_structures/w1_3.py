5c5816bfa75b92ec6a732852340e5321cd8ba063
abhira0 <abhishekr0@protonmail.com>
2021-05-14 18:37:31 +0530

    entire zeros edge case isnt passing for packet simulation

--- 2 Data Structures/week1_basic_data_structures/w1_3.py
+++ 2 Data Structures/week1_basic_data_structures/w1_3.py
@@ -5,7 +5,6 @@ class Queue:
         self.maxsize = maxsize
 
     def enqueue(self, key):
-        print(self.len, self.ma)
         if self.len >= self.maxsize:
             return -1
         self.queue.append(key)
@@ -24,34 +23,63 @@ class Queue:
         return self.queue[0]
 
 
-def process(requests, size):
-    q = Queue(size)
-    lost_ind = []
-    response = []
-    time = 0
-    processing_req = None
-    wait_until = 0
-    req_ind = 0
-    while True:
-        while requests[req_ind][0] == time:
-            if q.enqueue(requests[req_ind]) == -1:
-                lost_ind.append(req_ind)
-            req_ind += 1
-        if not processing_req and not q.isEmpty() and wait_until <= time:
-            processing_req = q.dequeue()
-            wait_until = time + processing_req[1]
+class Request:
+    def __init__(self, arrival, processing) -> None:
+        self.arrival = arrival
+        self.processing = processing
+        self.began_at = -1
+        self.completed_at = -1
 
+
+class RequestList:
+    def __init__(self) -> None:
+        self.ind = 0
+        self.requests = []
+        self.len = 0
+
+    def addRequest(self, req: Request):
+        self.requests.append(req)
+        self.len += 1
+
+    def getNext(self) -> Request:
+        if self.ind < self.len:
+            return self.requests[self.ind]
+
+    def isCompleted(self):
+        return self.ind == self.len
+
+
+def process(requests: RequestList, size):
+    q = Queue(size)
+    time = 0
+    processing_req: Request = None
+    wait_until = -1
+    while True:
+        while requests.getNext() and requests.getNext().arrival == time:
+            q.enqueue(requests.getNext())
+            requests.ind += 1
+        if wait_until == time and processing_req:
+            processing_req.completed_at = time
+            processing_req = None
+        while not processing_req and not q.isEmpty() and wait_until <= time:
+            processing_req = q.dequeue()
+            wait_until = time + processing_req.processing
+            processing_req.began_at = time
         time += 1
+        if requests.isCompleted() and q.isEmpty():
+            break
+    for resp in requests.requests:
+        print(resp.began_at)
 
 
 def main():
     size, n = map(int, input().split())
     if n == 0:
         return None
-    requests = []
+    requests = RequestList()
     for _ in range(n):
         a, p = map(int, input().split())
-        requests.append([a, p])
+        requests.addRequest(Request(a, p))
     process(requests, size)
 
 
